d5073d9c5d4b58c88e741ef71107c6e67d8d1418
Yonatan <yonatangenene30@gmail.com>
2026-06-04 11:02:15 +0300

    Complete exercise 1 middleware ordering and protected endpoint

--- Program.cs
+++ Program.cs
@@ -1,17 +1,33 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Authentication;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services
+    .AddAuthentication("Training")
+    .AddScheme<AuthenticationSchemeOptions, TrainingAuthHandler>("Training", null);
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
 app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGet("/api/assessments/results", () => Results.Ok(new
+{
+    courseCode = "CS-101",
+    studentId = "S-001",
+    letterGrade = "A"
+}))
+.RequireAuthorization();
 
 app.Run();
