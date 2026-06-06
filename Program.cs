45a733fcdbcefd0581e5753a67f51d5b721a4cc9
Yonatan <yonatangenene30@gmail.com>
2026-06-06 10:46:06 +0300

    Add payment options startup validation

--- Program.cs
+++ Program.cs
@@ -20,6 +20,11 @@ builder.Host.UseDefaultServiceProvider(options =>
 builder.Services.AddSingleton<EnrollmentWorker>();
 builder.Services.AddScoped<IEnrollmentService, EnrollmentService>();
 
+builder.Services.AddOptions<PaymentOptions>()
+    .BindConfiguration("Payments")
+    .ValidateDataAnnotations()
+    .ValidateOnStart();
+
 var app = builder.Build();
 app.UseMiddleware<RequestLoggingMiddleware>();
 
